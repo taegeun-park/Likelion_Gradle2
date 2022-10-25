974283928016ed82d243cfb6ade2d9e2f918339c
DESKTOP-RJC8TSU\taege <taegeun0525@gmail.com>
2022-10-25 10:44:41 +0900

    [22.10.25] UserDao

---
+++ src/main/java/com/db/dao/UserDao.java
@@ -0,0 +1,67 @@
+package com.db.dao;
+
+import com.db.domain.User;
+
+import java.sql.*;
+import java.util.Map;
+
+public class UserDao {
+    public void add(User user) {
+        Map<String, String> env = System.getenv();
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            Connection c = ConnectionMaker();
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
+
+            // Query문 실행
+            pstmt.executeUpdate();
+
+            pstmt.close();
+            c.close();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+        Map<String, String> env = System.getenv();
+        Connection c;
+        try {
+            // DB접속 (ex sql workbeanch실행)
+            c = DriverManager.getConnection(env.get("DB_HOST"),
+                    env.get("DB_USER"), env.get("DB_PASSWORD"));
+
+            // Query문 작성
+            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM users WHERE id = ?");
+            pstmt.setString(1, id);
+
+            // Query문 실행
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            User user = new User(rs.getString("id"), rs.getString("name"),
+                    rs.getString("password"));
+
+            rs.close();
+            pstmt.close();
+            c.close();
+
+            return user;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDao();
+//        userDao.add();
+        User user = userDao.findById("6");
+        System.out.println(user.getName());
+    }
+}
